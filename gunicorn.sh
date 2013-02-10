be27aa892ce2a39cc114d150dd6d40280bf2d438
Maxim Kislov <bibi@anorexiya.com>
2013-02-10 17:33:52 +0000

    asddas

--- gunicorn.sh
+++ gunicorn.sh
@@ -5,10 +5,10 @@ LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as
 USER=bibi
-GROUP=your_unix_group
+GROUP=bibi
 cd ~/findburo/findburo
-source ../bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec ../bin/gunicorn_django -w $NUM_WORKERS \
+exec gunicorn_django -w $NUM_WORKERS \
     --user=$USER --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
+    --bind localhost:8000
