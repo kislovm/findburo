fa8a93613d69c89a4fc067f11931f269029adf7e
Maxim <bibi@ubuntu.(none)>
2013-02-17 16:15:21 -0800

    починил, то что фильтр в js не ставил <p>

--- findburo/static/js/filters.js
+++ findburo/static/js/filters.js
@@ -1,11 +1,11 @@
 window.customFilters = {};
 
 customFilters.linebreaks = function(input) {
-    if(!input) return ''
-    var strings = input.split(/\n{2,}/); 
+    if(!input) return '';
+    var strings = input.split(/\r\n\r\n{1,}/); 
     result = ''; 
     for (var i=0;i < strings.length;i++){
-        result += ('<p>'+ strings[i].replace('\n', '<br \\>') +'</p>') 
+        result += ('<p>'+ strings[i].replace(/\n/g, '<br \\>') +'</p>') 
     }
     return result;
 }
